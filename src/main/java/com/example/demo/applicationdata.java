fced4aaf79ed1d538512e6bcb2a9b1e2312480cd
CR35777 <2404511547@qq.com>
2023-11-03 10:15:00 +0800

    最初的

---
+++ src/main/java/com/example/demo/applicationdata.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class applicationdata {
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationdata.class, args);
+    }
+
+}
